c5e5442f85026af85f6b13b7070830338aeef935
Skye M <skyem@hotmail.co.uk>
2015-03-19 20:16:33 +0000

    Started work on instruction cycles... I have no clue what I am doing...

--- src/main/java/uk/co/skyem/projects/Z80emu/Core.java
+++ src/main/java/uk/co/skyem/projects/Z80emu/Core.java
@@ -6,10 +6,12 @@ public class Core {
 	IBusDevice memoryBus;
 	IBusDevice IOBus;
 	Registers registers = new Registers();
+	InstructionDecoder instructionDecoder;
 
 	public Core(IBusDevice memory, IBusDevice IO) {
 		memoryBus = memory;
 		IOBus = IO;
+		instructionDecoder = new InstructionDecoder(this);
 		reset();
 	}
 
@@ -19,43 +21,11 @@ public class Core {
 		return (short) (memoryBus.getByte(address) & 0xFF | (memoryBus.getByte(address - 1) << 8 & 0xFF00));
 	}
 
-
 	public void reset() {
 		registers.clear();
 	}
 
 	public void cycle() {
-		short position = registers.getProgramCounter();
-		byte data = memoryBus.getByte(position);
-		// Whenever opcode is fetched
-		registers.incrementRefreshCounter();
-		// Convert the data into an instruction
-		// TODO: move this to a different class?
-		switch (data) {
-			case 0x00: // NOP
-				break;
-			case 0x01: // LD BC,nn
-				// Put 16 bits (nn) into register BC
-				registers.REG_BC.setData(read16bits((byte) (position + 2)));
-				registers.incrementProgramCounter((short) 2);
-				break;
-			case 0x02: // LD (BC),A
-				// Put the data in register A into the memory address specified in BC
-				memoryBus.putByte(registers.REG_BC.getData(), registers.REG_A.getData());
-				break;
-			case 0x03: // INC BC
-				// Increment (add one) to BC
-				registers.REG_BC.increment();
-				break;
-			case 0x06: // LD B,n
-				// Put 8 bits (n) into register B
-				registers.REG_B.setData(memoryBus.getByte(position + 1));
-				registers.incrementProgramCounter((short) 1);
-				break;
-			default:   // Error out
-				break;
-		}
-		registers.incrementProgramCounter();
-
+		instructionDecoder.cycle();
 	}
 }
--- src/main/java/uk/co/skyem/projects/Z80emu/InstructionDecoder.java
+++ src/main/java/uk/co/skyem/projects/Z80emu/InstructionDecoder.java
@@ -3,14 +3,79 @@ package uk.co.skyem.projects.Z80emu;
 import uk.co.skyem.projects.Z80emu.Register.*;
 import uk.co.skyem.projects.Z80emu.bus.IBusDevice;
 
-public abstract class InstructionDecoder {
-	IBusDevice memoryBus;
-	Core cpuCore;
+public class InstructionDecoder {
+	private IBusDevice memoryBus;
+	private Registers registers;
+	private Core cpuCore;
 
 	InstructionDecoder(Core cpu) {
 		memoryBus = cpu.memoryBus;
 		cpuCore = cpu;
+		registers = cpu.registers;
 	}
+
+	private byte currentOpcode;
+	private short position;
+	private int instuctionCycles;
+
+	// TODO: Is there a more appropriate name?
+	public void cycle() {
+		if (instuctionCycles > 0) { // Continue an instruction
+			--instuctionCycles;
+			switch (currentOpcode) {
+				case 0x00: // NOP
+					break;
+				case 0x01: // LD BC,nn
+					// No clue what to do...
+					break;
+				case 0x02: // LD (BC),A
+					// No clue what to do...
+					break;
+				case 0x03: // INC BC
+					// No clue what to do...
+					break;
+				case 0x06: // LD B,n
+					// No clue what to do...
+					break;
+				default:   // Error out
+					break;
+			}
+		} else { // Start a new instruction
+			position = registers.getProgramCounter();
+			currentOpcode = memoryBus.getByte(position);
+			switch (currentOpcode) {
+				case 0x00: // NOP
+					instuctionCycles = 3;
+					break;
+				case 0x01: // LD BC,nn
+					instuctionCycles = 9;
+					// Put 16 bits (nn) into register BC
+					registers.REG_BC.setData(cpuCore.read16bits((byte) (position + 2)));
+					registers.incrementProgramCounter((short) 2);
+					break;
+				case 0x02: // LD (BC),A
+					instuctionCycles = 6;
+					// Put the data in register A into the memory address specified in BC
+					memoryBus.putByte(registers.REG_BC.getData(), registers.REG_A.getData());
+					break;
+				case 0x03: // INC BC
+					instuctionCycles = 6;
+					// Increment (add one) to BC
+					registers.REG_BC.increment();
+					break;
+				case 0x06: // LD B,n
+					instuctionCycles = 6;
+					// Put 8 bits (n) into register B
+					registers.REG_B.setData(memoryBus.getByte(position + 1));
+					registers.incrementProgramCounter((short) 1);
+					break;
+				default:   // Error out
+					break;
+			}
+			registers.incrementProgramCounter();
+		}
+	}
+
 
 	private void LDRegisterFixed(Register8 destination, byte data) {
 		destination.setData(data);
