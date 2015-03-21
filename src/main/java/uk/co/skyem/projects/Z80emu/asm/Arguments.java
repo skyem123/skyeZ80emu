71a287b5ad4b61bb36ee1f01b007ac4be923ad7d
Victorious3 <Minefoto0@gmail.com>
2015-03-21 19:09:23 +0000

    Worked on instruction arguments for the assembler.

--- src/main/java/uk/co/skyem/projects/Z80emu/asm/Arguments.java
+++ src/main/java/uk/co/skyem/projects/Z80emu/asm/Arguments.java
@@ -1,19 +1,61 @@
 package uk.co.skyem.projects.Z80emu.asm;
 
-import sun.dc.pr.PRError;
 import uk.co.skyem.projects.Z80emu.Register;
 import uk.co.skyem.projects.Z80emu.Registers;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Arguments {
 
+	private ArrayList<Map.Entry<ArgumentType, Object>> arguments = new ArrayList<>();
+
 	private Arguments() {
 
+	}
+
+	@SuppressWarnings({"unchecked"})
+	public <T> T get(int index, ArgumentType<T> type) {
+		return (T) arguments.get(index).getValue();
+	}
+
+	public int size() {
+		return arguments.size();
+	}
+
+	public boolean isPresent(int index) {
+		return index > 0 && index < arguments.size();
+	}
+
+	public ArgumentType<?> getType(int index) {
+		return arguments.get(index).getKey();
+	}
+
+	public boolean isType(int index, ArgumentType<?> type) {
+		return getType(index) == type;
+	}
+
+	private <T> void add(ArgumentType<T> type, T object) {
+		arguments.add(new AbstractMap.SimpleImmutableEntry<>(type, object));
+	}
+
+	public static class ArgumentType<T> {
+
+		private ArgumentType() {
+
+		}
+
+		// Argument types
+		public static final ArgumentType<Reg>           REGISTER            = new ArgumentType<>();
+		public static final ArgumentType<Token.Label>   LABEL               = new ArgumentType<>();
+		public static final ArgumentType<Integer>       NUMBER              = new ArgumentType<>();
+		public static final ArgumentType<Reg>           POINTER_REGISTER    = new ArgumentType<>();
+		public static final ArgumentType<Token.Label>   POINTER_LABEL       = new ArgumentType<>();
+		public static final ArgumentType<Integer>       POINTER_NUMBER      = new ArgumentType<>();
+		public static final ArgumentType<String>        STRING              = new ArgumentType<>();
 	}
 
 	public static enum Reg {
@@ -24,25 +66,15 @@ public class Arguments {
 		}
 	}
 
-	public static class RegEntry {
-		public RegEntry(Reg register, boolean pointer) {
-
-		}
-	}
-
-	public static Object[] parse(String arguments) {
-		List<Object> arglist = new ArrayList<>();
-		for (String argument : arguments.split("\\s+")) {
+	public static Arguments parse(String args) {
+		Arguments arguments = new Arguments();
+		for (String argument : args.split("\\s+")) {
 			try {
-				arglist.add(Reg.valueOf(argument));
+				arguments.add(ArgumentType.REGISTER, Reg.valueOf(argument));
 			} catch (IllegalArgumentException e) {
 				// Parse everything else...
 			}
 		}
-		return arglist.toArray();
-	}
-
-	public static boolean matches(Class... clazz) {
-		return false;
+		return arguments;
 	}
 }
